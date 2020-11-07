4377c866ad34ccbf8809143d2985cef682a05fe1
ttoti87 <73862246+ttoti87@users.noreply.github.com>
2020-11-06 19:19:27 -1000

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,13 @@
 let myHeading = document.querySelector('h1');
 myHeading.textContent = 'batman et robin sont-ils gay?';
 document.querySelector('html').addEventListener('click', function(){alert('Aïe, arrêtez de cliquer !!');});
+let myImage = document.querySelector('img');
+
+myImage.addEventListener('click', function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/chat.png');
+    } else {
+      myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+});
