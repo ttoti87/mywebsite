387373d6a90b0f2651c4fd4a42422ea9031a80f8
ttoti87 <73862246+ttoti87@users.noreply.github.com>
2020-11-06 19:21:08 -1000

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,7 +2,6 @@ let myHeading = document.querySelector('h1');
 myHeading.textContent = 'batman et robin sont-ils gay?';
 document.querySelector('html').addEventListener('click', function(){alert('Aïe, arrêtez de cliquer !!');});
 let myImage = document.querySelector('img');
-
 myImage.addEventListener('click', function() {
     let mySrc = myImage.getAttribute('src');
     if (mySrc === 'images/firefox-icon.png') {
